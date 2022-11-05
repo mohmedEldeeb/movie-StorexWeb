c027e757a3aa3fcce87d6b9edc3a84850a814f54
Mohamed Eldeeb <79051634+mohmedEldeeb@users.noreply.github.com>
2022-11-05 16:38:43 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AddCategroyComponent } from './pages/add-categroy/add-categroy.component';
-import { AddMovieComponent } from './components/add-movie/add-movie.component';
-import { LoginComponent } from './pages/login/login.component';
-import { MovieComponent } from './pages/movie/movie.component';
-import { RegistrationComponent } from './pages/registration/registration.component';
+import { NgModule }                from '@angular/core';
+import { RouterModule, Routes }    from '@angular/router';
+import { AddCategroyComponent }    from './pages/add-categroy/add-categroy.component';
+import { AddMovieComponent }       from './components/add-movie/add-movie.component';
+import { LoginComponent }          from './pages/login/login.component';
+import { MovieComponent }          from './pages/movie/movie.component';
+import { RegistrationComponent }   from './pages/registration/registration.component';
 import { UpdateCategroyComponent } from './pages/update-categroy/update-categroy.component';
-import { AuthGuard } from './serves/auth/auth.guard';
-import { UbdateMovieComponent } from './pages/ubdate-movie/ubdate-movie.component';
+import { AuthGuard }               from './serves/auth/auth.guard';
+import { UbdateMovieComponent }    from './pages/ubdate-movie/ubdate-movie.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
-  {path:'registration',component:RegistrationComponent},
-  {path:'movie',component:MovieComponent ,canActivate:[AuthGuard]},
-  {path:'add-movie',component:AddMovieComponent ,canActivate:[AuthGuard]},
-  {path:'categroy',component:AddCategroyComponent ,canActivate:[AuthGuard]},
-  {path:'update-categroy/:id',component:UpdateCategroyComponent ,canActivate:[AuthGuard]},
-  {path:'update-movie/:id',component:UbdateMovieComponent ,canActivate:[AuthGuard]},
+  {path:'login',              component:LoginComponent                                   },
+  {path:'registration',       component:RegistrationComponent                            },
+  {path:'movie',              component:MovieComponent ,          canActivate:[AuthGuard]},
+  {path:'add-movie',          component:AddMovieComponent ,       canActivate:[AuthGuard]},
+  {path:'categroy',           component:AddCategroyComponent ,    canActivate:[AuthGuard]},
+  {path:'update-categroy/:id',component:UpdateCategroyComponent , canActivate:[AuthGuard]},
+  {path:'update-movie/:id',   component:UbdateMovieComponent ,    canActivate:[AuthGuard]},
   {path: '', redirectTo: '/movie', pathMatch: 'full'},
 ];
 
